5d439c393fac6459e400ce76486138173d6e6328
Rabii43 <Rabii12terres@gmail.com>
2025-04-19 14:48:15 -0700

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'auth', component: LoginComponent },
+  { path: '', redirectTo: '/auth', pathMatch: 'full' }, // Default route
+];
